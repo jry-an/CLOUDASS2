d067c618f1a6bccef8039677f0f290b6e08f9fe2
Aaron Chan <s3666603@student.rmit.edu.au>
2020-04-23 20:49:55 +1000

    Foundation of webpage

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+# render_template knows to search into a folder named templates
+from flask import Flask, render_template, url_for
 
+# Reference the current module which is application.py
 app = Flask(__name__)
 
-
+# The URL endpoint for a particular a thing in the app.
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -12,6 +14,7 @@ def home():
 def salvador():
     return "Hello, Salvador"
 
-
 if __name__ == "__main__":
+
+    # If we have any errors it'll pop up on the website
     app.run(debug=True)
