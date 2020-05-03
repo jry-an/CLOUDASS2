df0d328fc1f03896501c3c5443e80bb4323316e5
Aaron Chan <s3666603@student.rmit.edu.au>
2020-05-03 18:26:17 +1000

    map API working

--- application.py
+++ application.py
@@ -1,20 +1,50 @@
 # render_template knows to search into a folder named templates
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+import sqlalchemy
 
 # Reference the current module which is application.py
 app = Flask(__name__)
 
-# The URL endpoint for a particular a thing in the app.
+# example information to test map API, I will update this with Google cloud MYSQL
+class School:
+    def __init__(self, key, name, lat, lng):
+        self.key  = key # Identifier for each object
+        self.name = name
+        self.lat  = lat
+        self.lng  = lng
+
+universities = (
+    School('rmit',      'RMIT',   -37.808125, 144.962701),
+    School('monash', 'Monash',    -37.910545, 145.136246),
+    School('sb',     'Swinburne', -37.822146, 145.038955)
+)
+
+# lookup by key by creating a dictionary, for every object in universities
+# we create a school object inside uni_by_key
+uni_by_key = {school.key: school for school in universities}
+
+
 @app.route("/")
-def home():
-    return render_template('home.html')
+def index():
 
+    # Passing in schools tuples
+    return render_template('home.html', universities=universities)
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+# Anything following the / is to be passed into the function below
+@app.route("/<school_code>")
 
-if __name__ == "__main__":
+# Using the passed in variable school_code into the function
+def show_school(school_code):
 
-    # If we have any errors it'll pop up on the website
-    app.run(debug=True)
+    # Using school_code to lookup in the uni_by_key dictionary
+    school = uni_by_key.get(school_code)
+
+    # If Found
+    if school:
+        return render_template('map.html', school=school)
+
+    # If not found (Page not found)
+    else:
+        abort(404)
+
+app.run(host='localhost', debug=True)
